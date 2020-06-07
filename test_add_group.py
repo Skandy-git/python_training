71e25933b26f45fcb339cf094e82ced8fef23cbd
Nikita Skvortsov <skvortsov.11@mail.ru>
2020-06-07 12:57:29 +0300

    Добавлены вспомогальные методы, повышена читаемость кода

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -14,30 +11,51 @@ class TestAddGroup(unittest.TestCase):
 
     
     def test_add_group(self):
-        ws = self.wd
-        ws.get("http://localhost/addressbook/")
-        ws.find_element_by_name("user").click()
-        ws.find_element_by_name("user").clear()
-        ws.find_element_by_name("user").send_keys("admin")
-        ws.find_element_by_name("pass").click()
-        ws.find_element_by_name("pass").clear()
-        ws.find_element_by_name("pass").send_keys("secret")
-        ws.find_element_by_xpath("//input[@value='Login']").click()
-        ws.find_element_by_link_text("groups").click()
-        ws.find_element_by_name("new").click()
-        ws.find_element_by_name("group_name").click()
-        ws.find_element_by_name("group_name").clear()
-        ws.find_element_by_name("group_name").send_keys("123")
-        ws.find_element_by_name("group_header").click()
-        ws.find_element_by_name("group_header").clear()
-        ws.find_element_by_name("group_header").send_keys("123")
-        ws.find_element_by_name("group_footer").click()
-        ws.find_element_by_name("group_footer").clear()
-        ws.find_element_by_name("group_footer").send_keys("123")
-        ws.find_element_by_name("submit").click()
-        ws.find_element_by_link_text("group page").click()
-        ws.find_element_by_link_text("Logout").click()
-    
+        wd = self.wd
+        self.Open_home_page(wd)
+        self.Login(wd)
+        self.Open_group_Pages(wd)
+        self.Create_group(wd)
+        self.Return_to_groups_page(wd)
+        self.Logout(wd)
+
+    def Logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def Return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def Create_group(self, wd):
+        # Init group creation
+        wd.find_element_by_name("new").click()
+        # Fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("123")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("123")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("123")
+        # Submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def Open_group_Pages(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def Login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def Open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
@@ -55,3 +73,5 @@ class TestAddGroup(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
