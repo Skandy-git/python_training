623a9fc11da5ab10f731e2d3c51381f8d5bd18af
Nikita Skvortsov <skvortsov.11@mail.ru>
2020-06-21 12:51:58 +0300

    Добавлен переход на страницу контактов перед подсчётом количества контактов в методе open_home_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,7 +8,8 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_link_text("//input[@value='Delete']")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def contact(self, contact):
         wd = self.app.wd
