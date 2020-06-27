ddbd1455184628b0257413da09e91e26d38e23a4
Nikita Skvortsov <skvortsov.11@mail.ru>
2020-06-27 18:12:20 +0300

    Загружена информация групп из приложения в виде списков

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -4,4 +4,7 @@ from model.group import Group
 def test_Delete_Group(app):
     if app.group.count() == 0:
         app.group.create_group(Group(name="123", header="123", footer="123"))
+    old_groups = app.group.get_group_list()
     app.group.delete_group()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -4,6 +4,9 @@ from model.contact import Contact
 def test_update_contact(app):
     if app.contact.count() == 0:
         app.contact.contact(Contact(firstname="Nikita", middlename="qqqqq", lastname="Skvortsov"))
+    old_groups = app.group.get_group_list()
     app.contact.update_contact(Contact(firstname="TEST", middlename="TEST", lastname="TEST", nickname="TEST",
                                        mobile="TEST", work="TEST", fax="3TEST", email="TEST",
                                        email2="TEST", homepage="TEST", byear="2000"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
