80f4b3a5cbb0f82bea96f56d5597662d18e1adab
Nikita Skvortsov <skvortsov.11@mail.ru>
2020-07-08 17:03:17 +0300

    Параметризованы тесты для создания групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -27,7 +27,7 @@ class Contact:
         self.address = address
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.middlename, self.nickname, self.email, self.email2)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == self.firstname and self.lastname == self.lastname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
-
+import pytest
+import random
+import string
 from model.contact import Contact
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdatas = [Contact(firstname="", middlename="", lastname="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10),
+            nickname=random_string("nickname", 10), email=random_string("email", 10), email2=random_string("email2", 10)
+            )
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdatas, ids=[repr(x) for x in testdatas])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Nikita", middlename="qqqqq", lastname="Skvortsov", nickname="sssss",
-                                mobile="11111111111", work="2222222222", fax="3333333", email="test1@mail.ru",
-                                email2="test2@gmail.com", homepage="qweasd.ru", byear="1990")
     app.contact.contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contact = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
-
-
-#def test_add_pers_info(app):
-#    app.contact.contact(Contact(firstname="Nikita", middlename="qqqqq", lastname="Skvortsov"))
