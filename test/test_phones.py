2e150e0fabb4e96491d576db811d4e662bd41934
nikita.skvortsov@icl-services.com <hv74xgt@>
2022-05-25 16:07:23 +0300

    Update requirements.txt

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,9 @@
 import re
 from random import randrange
-import pytest
+from pytest import mark
 
 
-@pytest.mark.skip()
+@mark.skip()
 def test_phones_on_homepage(app):
     index = randrange(len(app.contact.get_contact_list()))
     contact_from_homepage = app.contact.get_contact_list()[index]
@@ -11,7 +11,7 @@ def test_phones_on_homepage(app):
     assert contact_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
 
 
-@pytest.mark.skip()
+@mark.flaky(reruns=2, reruns_delay=3)
 def test_phones_on_contact_view_page(app):
     contact_from_view = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
